8b3cceb9212fb2e777ecaf27a55a1374aec36e20
Hitesh Patel <hiteshprayosha79@gmail.com>
2022-06-07 17:13:33 +0100

    Apply dockerisation

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # spring-crud-hibernate
 
 
-Commnad is to package the applicaiton as JAR file
 
-./mvnw clean package -DskipTests
 
 Run the application in local env.
 
@@ -20,3 +18,27 @@ git remote add origin git@github.com:hiteshprayosha/spring-crud-hibernate.git
 git push -u -f origin main
 
 
+Dockerazation
+
+1. Commnad is to package the applicaiton as JAR file
+./mvnw clean package -DskipTests
+
+2. Let's make the new src/main/docker directory. After that, we copy the application JAR file there:
+cp target/spring-crud-hibernate-0.0.1-SNAPSHOT.jar src/main/docker/
+
+3. Finally, we create this Dockerfile in that same directory:
+
+4. Let's write Docker Compose file, docker-compose.yml
+5. command to run the applicaion in docker
+cd src/main/docker
+docker-compose up
+
+6. After making any chnage in code to do following commnads
+cd src/main/docker
+docker-compose down
+docker rmi spring-crud-hibernate:latest
+docker-compose up
+
+7. Using postman, call below endpoints (find postman folder)
+GET http://localhost:8080/api/v1/employees
+POST http://localhost:8080/api/v1/employees
--- src/main/java/io/hitesh/SpringCrudlHibernateApplication.java
+++ src/main/java/io/hitesh/SpringCrudlHibernateApplication.java
@@ -1,13 +1,48 @@
 package io.hitesh;
 
+import io.hitesh.model.Employee;
+import io.hitesh.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.List;
 
 @SpringBootApplication
+@Slf4j
+@EnableScheduling
 public class SpringCrudlHibernateApplication {
+
+	@Autowired
+	private EmployeeRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCrudlHibernateApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void runAfterStartup(){
+		queryAllEmployee();
+		createEmployee();
+		queryAllEmployee();
+	}
+
+	private void createEmployee() {
+		Employee employee = new Employee();
+		employee.setFirstName("John");
+		employee.setLastName("Smith");
+		employee.setEmail("h.smith@email.com");
+		log.info("Saving new employee...");
+		this.repository.save(employee);
+	}
+
+	private void queryAllEmployee() {
+		List<Employee> allEmployee = this.repository.findAll();
+		log.info("Number of employees: {}", allEmployee.size());
 	}
 
 }
